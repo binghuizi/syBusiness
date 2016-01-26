c606ae41deaa9c7a71474f3e3fd57815d6c24b44
赵佳慧--芒果iOS <1146623752@qq.com>
2016-01-26 20:18:57 +0800

    Initial Commit

---
+++ BusinessWeekly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BusinessWeekly
+//
+//  Created by scjy on 16/1/26.
+//  Copyright © 2016年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
